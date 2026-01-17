dcad4f842af1e6ac45ba341eb10cd5fecba54e41
Bharath <91795938+Bharath200415@users.noreply.github.com>
2026-01-17 12:06:49 +0530

    Time: 2 ms (33.60%) | Memory: 43 MB (57.82%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,36 +1,30 @@
 class Solution {
-    public int findSplits(int[] nums, int midsum){
-        int splits = 1;
-        int initial = 0;
-        for (int i=0;i<nums.length;i++){
-            if (initial+nums[i]<=midsum){
-                initial+=nums[i];
-            }else{
-                splits+=1;
-                initial=nums[i];
-            }
-        }
-        return splits;
-    }
-
     public int splitArray(int[] nums, int k) {
         int start = 0;
         int end = 0;
-        for (int i=0;i<nums.length;i++){
-            start = Math.max(start,nums[i]);
-            end+=nums[i];
+        for (int num:nums){
+            start = Math.max(num,start);
+            end+=num;
         }
-
         while (start<=end){
+            int sum = 0;
+            int splits=1;
             int mid = start+(end-start)/2;
-            int splitNum = findSplits(nums,mid);
-            if (splitNum>k){
+
+            for (int num:nums){
+                if (num+sum>mid){
+                    splits+=1;
+                    sum = num;
+                }else{
+                    sum+=num;
+                }
+            }
+            if (splits>k){
                 start = mid+1;
             }else{
                 end = mid-1;
             }
         }
         return start;
-        
     }
 }

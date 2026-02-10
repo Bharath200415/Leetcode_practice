cf184c0d52cf9081b5afba66c2afbd3e6afc30b1
Bharath <91795938+Bharath200415@users.noreply.github.com>
2026-02-10 23:16:01 +0530

    Time: 374 ms (33.39%) | Memory: 47.2 MB (27.01%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
@@ -2,14 +2,14 @@ class Solution {
     public int longestBalanced(int[] nums) {
         int res=0;
         for (int i=0;i<nums.length-1;i++){
-            HashSet<Integer> h1= new HashSet<>();
-            HashSet<Integer> h2 = new HashSet<>();
+            Set<Integer> h1= new HashSet<>();
+            Set<Integer> h2 = new HashSet<>();
             if (nums[i]%2==0){
                 h1.add(nums[i]);
             }else{
                 h2.add(nums[i]);
             }
-            for (int j=i+1;j<nums.length;j++){
+            for (int j=i;j<nums.length;j++){
                 if (nums[j]%2==0){
                     h1.add(nums[j]);
                 }else{
